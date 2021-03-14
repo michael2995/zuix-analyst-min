32aa59805e7e62b75320ef52a574b32411e16501
michael2995 <msg@zigbang.com>
2021-03-14 14:11:12 +0900

    chore: add log to report

--- src/report.ts
+++ src/report.ts
@@ -14,21 +14,18 @@ export const reportToServer = (params: ReportParams) => {
         throw Error("ZUIX_SERVER_ENDPOINT is not provided")
     }
 
-    try {
-        fetch(params.server, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                commitHash: params.commit,
-                contributor: params.contributor,
-                data: params.report
-            })
+    fetch(params.server, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            commitHash: params.commit,
+            contributor: params.contributor,
+            data: params.report
         })
-    } catch (e) {
-        throw Error(e)
-    } finally {
-        console.log("Closing Report")
-    }
+    })
+        .then((res) => res.text())
+        .then((txt) => console.log(`Server responded with ${txt}`))
+        .catch((err) => console.error(err))
 }
